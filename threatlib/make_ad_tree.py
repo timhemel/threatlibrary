eaa72c2ae80d861acb6d584c2020208b59880b71
Tim Hemel <tim@securesoftware.nl>
2019-10-11 21:29:48 +0200

    Add TlObjects on top of markdown document

--- threatlib/make_ad_tree.py
+++ threatlib/make_ad_tree.py
@@ -1,37 +1,65 @@
 
 import sys
 
-from read_tl import ThreatlibraryReader, extract_links_from_list
+from read_tl import MarkdownDocumentReader, Threatlibrary, extract_links_from_list
 from graphviz import Digraph
 
-def render_or_node(g, title, content):
-    g.node(title)
+render_node_attrs = {
+        'threat': { 'shape': 'Mrecord',
+            'style': 'filled',
+            'color': '#7f0808',
+            'fillcolor': '#f3a1a1',
+            'linecolor': 'red'
+        },
+        'mitigation': { 'shape': 'record',
+            'style': 'filled',
+            'color': '#087f20',
+            'fillcolor': '#81c68f',
+            'linecolor': 'green'
+        }
+}
 
-def render_threat(g, title, content):
-    print('threat', title)
-    variants = content.get('Variants')
-    if variants is not None:
-        render_or_node(g, title, content)
-        for v in extract_links_from_list(variants):
-            g.edge(title, v)
-    pass
+def render_node(g, title, node_operator, node_attrs):
+    if node_operator == 'and':
+        label = "{ <f> %s | <and> &}" % title
+    else:
+        label = "<f> %s" % title
+    g.node(title, label=label, _attributes=node_attrs)
 
-def render_mitigation(g, title, content):
-    print('mitigation', title)
+def render_edge(g, source, destination):
+    print(source,destination)
+    g.edge(source, destination)
+
+def render_obj(g, tl_obj):
+    attrs = render_node_attrs.get(tl_obj.get_node_type())
+    operator = tl_obj.get_node_operator()
+    render_node(g, tl_obj.get_name(), operator, attrs)
+
+    if operator == 'or':
+        for v in tl_obj.get_variants():
+            render_edge(g, tl_obj.get_name(), v)
+    elif operator == 'and':
+        for v in tl_obj.get_steps():
+            render_edge(g, tl_obj.get_name(), v)
+
+    for v in tl_obj.get_mitigations():
+        print('x',v)
+        render_edge(g, tl_obj.get_name(), v)
+
+    for v in tl_obj.get_threatened_mitigations():
+        print('y',v)
+        render_edge(g, v, tl_obj.get_name())
 
 def main():
-    tl_reader = MarkdownDocumentReader()
-    tl_reader.from_file(sys.stdin)
-    library = tl_reader.get_document_name()
+    doc_reader = MarkdownDocumentReader()
+    doc_reader.from_file(sys.stdin)
+    library = Threatlibrary()
+    library.from_markdown_document_reader(doc_reader)
+    # library = tl_reader.get_document_name()
     g = Digraph('G', engine='dot')
-    for title, content in tl_reader.get_sections():
-        # is this an attack or a defense?
-        if content.get('Threat') is not None:
-            render_threat(g, title, content)
-            # threat
-        else:
-            # mitigation
-            render_mitigation(g, title, content)
+    for tl_obj in library.get_tl_objects():
+        print(tl_obj)
+        render_obj(g, tl_obj)
     g.view()
 
 if __name__=="__main__":
--- threatlib/read_tl.py
+++ threatlib/read_tl.py
@@ -27,8 +27,85 @@ def extract_links_from_list(s):
         if x is not None:
             yield x
 
-class ThreatlibraryReader:
-    pass
+class TlObject:
+
+    def __init__(self, name, sections):
+        self.name = name
+        self.determine_node_operator(sections)
+
+    def get_name(self):
+        return self.name
+
+    def determine_node_operator(self, sections):
+        self.variants = list(extract_links_from_list(
+            sections.get('Variants', '')))
+        self.steps = list(extract_links_from_list(sections.get('Steps', '')))
+        if self.variants != [] and self.steps != []:
+            raise Exception('Threat cannot have Variants and Steps simultaneously')
+        if self.variants != []:
+            self.node_operator = 'or'
+        elif self.steps != []:
+            self.node_operator = 'and'
+        else:
+            self.node_operator = 'leaf'
+
+    def get_node_operator(self):
+        return self.node_operator
+
+    def get_node_type(self):
+        return self.node_type
+
+    def get_variants(self):
+        return self.variants
+
+    def get_steps(self):
+        return self.steps
+
+    def get_mitigations(self):
+        return []
+
+    def get_threatened_mitigations(self):
+        return []
+ 
+class TlThreat(TlObject):
+    node_type = 'threat'
+
+    def __init__(self, name, sections):
+        super().__init__(name, sections)
+        self.mitigations = list(extract_links_from_list(
+            sections.get('Suggested Mitigation','')))
+        self.threatened_mitigations = list(extract_links_from_list(
+            sections.get('Threatens','')))
+
+    def get_mitigations(self):
+        return self.mitigations
+
+    def get_threatened_mitigations(self):
+        return self.threatened_mitigations
+
+class TlMitigation(TlObject):
+    node_type = 'mitigation'
+
+class Threatlibrary:
+
+    def __init__(self):
+        self.tl_objects = []
+
+    def from_markdown_document_reader(self, doc_reader):
+        self.name = doc_reader.get_document_name()
+        for title, content in doc_reader.get_sections():
+            self.add_tl_object(title, content)
+
+    def add_tl_object(self, title, content):
+        print(title, content)
+        if content.get('Threat') is not None:
+            tl_obj = TlThreat(title, content)
+        else:
+            tl_obj = TlMitigation(title, content)
+        self.tl_objects.append(tl_obj)
+
+    def get_tl_objects(self):
+        return self.tl_objects
 
 class MarkdownDocumentReader:
     def __init__(self):
